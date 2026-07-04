c8d6a85b47e78e9c859dee1a6174c63a14161cbb
rosenlo <rosenpy@gmail.com>
2026-07-04 15:56:26 +0800

    fix: guard pyenv/cargo, fix starship time format

--- .zshrc.d/30-env.zsh
+++ .zshrc.d/30-env.zsh
@@ -6,10 +6,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
 
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+command -v pyenv &>/dev/null && eval "$(pyenv init -)" && eval "$(pyenv virtualenv-init -)"
 
-source $HOME/.cargo/env
+[ -f "$HOME/.cargo/env" ] && source "$HOME/.cargo/env"
 
 export KUBECONFIG=$HOME/.kube/config
 
