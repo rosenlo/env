41a3db23a1c5446fc9ad3812a5a479b4b0a4e04f
rosenlo <rosenpy@gmail.com>
2026-07-04 15:43:32 +0800

    feat: install oh-my-zsh and zsh-autosuggestions plugin automatically

--- install.sh
+++ install.sh
@@ -24,6 +24,22 @@ for file in .vimrc .zshrc; do
   echo "    linked $file"
 done
 
+echo "==> Installing oh-my-zsh..."
+if [ -d "$HOME/.oh-my-zsh" ]; then
+  echo "    already installed, skipping"
+else
+  RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+echo "    installing zsh-autosuggestions plugin..."
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+AUTOSUGGEST_DIR="$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+if [ -d "$AUTOSUGGEST_DIR" ]; then
+  git -C "$AUTOSUGGEST_DIR" pull
+else
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$AUTOSUGGEST_DIR"
+fi
+
 echo "==> Installing vim-plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
